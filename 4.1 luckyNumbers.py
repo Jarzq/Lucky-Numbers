0c9c5f5fa9785f998472cb6af004364a8eb28037
Jarzq <78604811+Jarzq@users.noreply.github.com>
2021-02-05 15:53:05 +0100

    Add files via upload

---
+++ 4.1 luckyNumbers.py
@@ -0,0 +1,40 @@
+
+def luckyNumbers(n, lucktab):   # funkcja zwracająca informację czy liczba jest liczbą szczęśliwą czy nie(przeszukujemy listę luckyTab)
+    for liczba in lucktab:
+        if n==liczba:
+            return True
+    return False
+
+
+
+counter = 0                      # licznik indeksu liczby, która jest aktualnie najnowszą szczęśliwą
+luckyTab = []                    # lista przechowująca szczęśliwe liczby
+luckyTabCurrent = []             # lista do której w danej iteracji bądą wpisywane liczby poza wykluczonymi
+
+for i in range(1, 10001):        # pętla dzięki której na początku uzyskamy w LuckyTab same liczby nieparzyste
+    if (i % 2 != 0):
+            luckyTab.append(i)
+counter += 1
+newLuckier = luckyTab[counter]  # przypisanie do zmiennej newLuckier najnowszej szczęśliwej liczby
+
+while newLuckier < len(luckyTab):                   # cała pętla będzie się wykonywała dopóki najnowsza szczęśliwa liczba będzie mniejsza od długości listy
+    for i in range(0, len(luckyTab)):               # tworzymy pętle równą długości listy
+        if (i + 1) % newLuckier != 0:               # wyrzucamy indeksy o wielokrotności ostatniej szczęśliwej liczby(zgodnie z założeniem zadania)
+            luckyTabCurrent.append(luckyTab[i])
+
+    luckyTab = luckyTabCurrent.copy()               # przypisuję tablicy luckyTab wartości zaaktualizowane
+    luckyTabCurrent.clear()                         # czyszczę tablicę pomocniczą, żeby była gotowa na kolejny obrót pętli ;)
+    counter += 1                                    # za każdym obrotem pętli zwiększam licznik o 1
+    newLuckier = luckyTab[counter]
+
+
+
+with open("dane (1).txt", "r") as file:
+    count = 0                                         # zeruję zmienną count, która będzie liczyła ilość liczb spełniających warunek
+
+    for line in file:
+        if luckyNumbers(int(line), luckyTab) == True:   # sprawdzam każdą liczbę osobno czy jest szczęśliwa
+            count += 1
+
+
+    print("liczb, które są  szczęśliwe jest w pliku: ", count)              # wyświetlam wynik 307
